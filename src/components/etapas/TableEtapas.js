18a2710e4af52769310a2cceb141966549045e30
unknown <brunovallejorj@gmail.com>
2022-11-06 15:24:36 -0300

    tabela quase concluida

---
+++ src/components/etapas/TableEtapas.js
@@ -0,0 +1,19 @@
+
+function EtapaRow (props){
+    return (
+        <tr>
+            <td>{props.ordem}</td>    
+            
+            <td>{props.nome}</td>
+
+             <td>{props.duracao}</td>  
+        </tr>
+    )
+
+
+
+
+
+}
+
+export default EtapaRow
--- src/pages/Etapas.js
+++ src/pages/Etapas.js
@@ -1,23 +1,30 @@
-
+import EtapaRow from "../components/etapas/TableEtapas"
 import React from "react"
 import {Table} from "react-bootstrap"
 function Etapas () {
     return (
 
-      <div>
+      <div className="container mt-4">
         <h1>Etapas Produtivas</h1>
         <Table>
           <thead>
             <tr>
+              <th>Ordem</th>
               <th>Nome</th>
               <th>Duração</th>
+              <th></th>
 
             </tr>
-            
+
 
           </thead>
 
+            <tbody>
+              <EtapaRow ordem={1} nome={"B"} duracao={1} /> 
+              <EtapaRow ordem={2} nome={"C"} duracao= {2}/>
+            
 
+            </tbody>
 
         </Table>
 
