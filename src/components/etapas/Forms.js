472a67dec9c723e42b9382bc54070f6445aced24
unknown <brunovallejorj@gmail.com>
2022-11-07 21:27:13 -0300

    tabela interativa etapas

---
+++ src/components/etapas/Forms.js
@@ -0,0 +1,36 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {useState} from 'react';
+
+function Forms(props) {
+  const [nome, setNome] = useState()
+  const [duracao, setDuracao] = useState()
+
+  function retornaEtapa(){
+    let etapa = {nome, duracao}
+    props.novoHandler(etapa)
+  }
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formNome">
+        <Form.Label>Nome</Form.Label>
+        <Form.Control value={nome} onChange={(e) => setNome(e.target.value)} type="string" placeholder="Digite o nome da etapa" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formDuracao">
+        <Form.Label>Duração</Form.Label>
+        <Form.Control value={duracao} onChange={(e) => setDuracao(e.target.value)} type="string" placeholder="Digite a duração da etapa" />
+      </Form.Group>
+
+
+
+      <Button variant="primary" className='m-2' onClick={retornaEtapa}>
+        Criar Etapa
+      </Button>
+
+    </Form>
+  );
+}
+
+export default Forms;
--- src/pages/Etapas.js
+++ src/pages/Etapas.js
@@ -1,48 +1,33 @@
-import EtapaRow from "../components/etapas/TableEtapas"
-import React, { useState } from "react"
-import {Button, Table} from "react-bootstrap"
-function Etapas () {
-const [etapas,setEtapas]= useState([])
-  function AdicionarEtapa(){
-    
+import { useState } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Table from '../components/etapas/Table.js'
+import { Button } from 'react-bootstrap';
+import NovaEtapa from '../components/etapas/NovaEtapa.js';
 
-
-
+function Etapas() {
+  const [NEtapasModal, setNEtapasModal] = useState(false)
+  const [etapas, setEtapas] = useState([])
+  function NovaEtapaModal() {
+    setNEtapasModal(true)
   }
-    return (
+  function NovaEtapaModalClose(){
+    setNEtapasModal(false)
+  }
+  function novoHandler2(etapa){
+    setEtapas([...etapas, etapa])
+  }
+  return (
+    <div className='container mt-3'>
+      <NovaEtapa show={NEtapasModal} cHandler={NovaEtapaModalClose} novoHandler2={novoHandler2}/>
+      <div className='d-flex flex-row justify-content-between w-100 mb-3'>
+          <h1>Etapa</h1>
+          <Button className="m-2" onClick={NovaEtapaModal}>Nova Etapa</Button>
+        </div>
+      <StyledEngineProvider injectFirst>
+        <Table rows={etapas} />
+      </StyledEngineProvider>
+    </div>
+  )
+};
 
-      <div className="container mt-4">
-        <div className="d-flex flex-row justify-content-between">
-          <h1>Etapas Produtivas</h1>
-          <Button className="mt-2 mb-2">Adicionar</Button>
-          </div>
-
-        <Table>
-          <thead>
-            <tr>
-              <th>Ordem</th>
-              <th>Nome</th>
-              <th>Duração</th>
-              <th></th>
-
-            </tr>
-
-
-          </thead>
-
-            <tbody>
-              <EtapaRow ordem={1} nome={"B"} duracao={1} /> 
-              <EtapaRow ordem={2} nome={"C"} duracao= {2}/>
-            
-
-            </tbody>
-
-        </Table>
-
-
-
-      </div>
-    )
-  };
-  
-  export default Etapas;
+export default Etapas;
